ed68c964fa7133a6ec947b6efbf2437fdcbb998f
ZAA18 <Zubuhle01@gmail.com>
2026-06-03 12:49:35 +0200

    Working on Navigation.js

---
+++ Portfolio root/Scripts javascript/navigation.js
@@ -0,0 +1,5 @@
+document.getElementById("homeBtn").addEventListener("click", () =>
+     {window.location.href = "index.html";});
+
+document.getElementById("aboutBtn").addEventListener("click", () =>
+{window.location.href= "Pages/about.html"});
