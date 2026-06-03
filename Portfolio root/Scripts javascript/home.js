22cf8105657ea8733acb0195cbb7c5344e046f4b
ZAA18 <Zubuhle01@gmail.com>
2026-06-03 13:33:26 +0200

    Added Navigation quite well on javascript about to put things in order so that my site looks like the wireframe i made

---
+++ Portfolio root/Scripts javascript/home.js
@@ -0,0 +1,5 @@
+document.getElementById("viewProjectsBtn").addEventListener("click", ()=>
+ {window.location.href = "projects.html";});
+
+document.getElementById("aboutMeBtn").addEventListener("click", () => 
+{window.location.href = "about.html";});
--- Portfolio root/Scripts javascript/navigation.js
+++ Portfolio root/Scripts javascript/navigation.js
@@ -2,4 +2,19 @@ document.getElementById("homeBtn").addEventListener("click", () =>
      {window.location.href = "index.html";});
 
 document.getElementById("aboutBtn").addEventListener("click", () =>
-{window.location.href= "Pages/about.html"});
+{window.location.href= "Pages/about.html";});
+
+document.getElementById("projectsBtn").addEventListener("click", ()=>
+{window.location.href = "Pages/projects.html";});
+
+document.getElementById("contactBtn").addEventListener("click", ()=> 
+{window.location.href = "Pages/contact.html";});
+
+document.getElementById("githubBtn").addEventListener("click", ()=> 
+{window.open("http://github.com/ZAA18", "_blank");});
+
+document.getElementById("linkedinBtn").addEventListener ("click", ()=>
+{window.open("http://linkedin.com", "_blank");});
+
+document.getElementById("itchBtn").addEventListener("click", () => 
+{window.open("https://Zubuhle-nel.itch.io");});
