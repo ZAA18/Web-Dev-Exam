46121c6245edee9d8e06c3ca7cd9c6da731d86c4
ZAA18 <Zubuhle01@gmail.com>
2026-06-09 00:43:06 +0200

    Added Menu instead of uppercase MENU Added links to the projects buttons update links on the footer buttons

--- Portfolio root/Scripts javascript/contact.js
+++ Portfolio root/Scripts javascript/contact.js
@@ -136,3 +136,13 @@ const isValid = validateFirstName() && validateLastName() && validateEmail() && 
 if (isValid)
     {alert("form submitted successfully!");} 
 });
+
+// footer
+document.getElementById("footerGithubBtn").addEventListener("click", ()=> 
+    {window.open("http://github.com/ZAA18", "_blank");});
+    
+    document.getElementById("footerLinkedinBtn").addEventListener ("click", ()=>
+    {window.open("http://linkedin.com", "_blank");});
+    
+    document.getElementById("footerItchBtn").addEventListener("click", () => 
+    {window.open("https://Zubuhle-nel.itch.io", "_blank");}); 
