fd4e045f32d9c7fa459f1105e5188f31fda832b8
ZAA18 <Zubuhle01@gmail.com>
2026-06-08 15:04:43 +0200

    Added reveal interaction

--- Portfolio root/Scripts javascript/home.js
+++ Portfolio root/Scripts javascript/home.js
@@ -1,5 +1,34 @@
-document.getElementById("viewProjectsBtn").addEventListener("click", ()=>
+/*document.getElementById("viewProjectsBtn").addEventListener("click", ()=>
  {window.location.href = "projects.html";});
 
 document.getElementById("aboutMeBtn").addEventListener("click", () => 
 {window.location.href = "about.html";});
+*/
+
+const elements = document.querySelectorAll(
+    ".hero, .hero-image, .featured-projects, .project-card"
+);
+
+function revealElements()
+{
+    elements.forEach((element) =>
+    {
+        const top =
+            element.getBoundingClientRect().top;
+
+        const windowHeight =
+            window.innerHeight;
+
+        if(top < windowHeight - 100)
+        {
+            element.classList.add("show");
+        }
+    });
+}
+
+window.addEventListener(
+    "scroll",
+    revealElements
+);
+
+revealElements();
